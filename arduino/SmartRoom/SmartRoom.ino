1c1e2ee255cae0fc9898ce92e9f0e7b5a035ccff
jeremy trips <Jeremy.trips@gmail.com>
2020-02-10 12:36:21 +0100

    Adding info led logic

--- arduino/SmartRoom/SmartRoom.ino
+++ arduino/SmartRoom/SmartRoom.ino
@@ -1,7 +1,12 @@
 #include <EEPROM.h>
 #include "WiFi.h"
 
-// Constal
+// Pinout declaration for the info RGB pin. Color will be described in the table doc.
+#define infoPinR 13  
+#define infoPinG 14
+#define infoPinB 15
+
+// Variable declaration.
 char ssid[50];
 char password[50];
 
@@ -14,21 +19,42 @@ void setupWifiData();
 void setup() {
   // put your setup code here, to run once:
   pinMode(2, OUTPUT);
+
+  pinMode(infoPinR, OUTPUT);
+  pinMode(infoPinG, OUTPUT);
+  pinMode(infoPinB, OUTPUT);
+
+  // Pin needs to be high for the led to be off as it is anode one.
+  digitalWrite(infoPinR, HIGH);
+  digitalWrite(infoPinG, HIGH);
+  digitalWrite(infoPinB, HIGH);
+
   EEPROM.begin(512);
   if(EEPROM.read(511) != 0x55){
     // Start Serial comunication for setup.
     setupWifiData();
   } else {
-    // Start the wifi protocol comunication.
-        
+    /* Start the wifi protocol comunication.
+     * The blue led will blink until the connection occurs. 
+     * After 10 try, the esp switch on a red led to warn the user. 
+     */
+    int connection_try = 0;
     EEPROM_read(ssid, 401);
     EEPROM_read(password, 451);
     
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
+      // Checking the wifi status every 500 ms.
       delay(500);
-      digitalWrite(2, not digitalRead(2));
+      connection_try++;
+      if(connection_try>10){
+        // Error as occurs the SSID or password may be wrong. 
+        digitalWrite(infoPinR, !digitalRead(infoPinR));
+      } else {
+        digitalWrite(infoPinB, !digitalRead(infoPinB));
+      }
     }
+    digitalWrite(infoPinB, HIGH);
   }
 
   EEPROM.end();
@@ -44,22 +70,37 @@ void setupWifiData(){
    * Normaly the first boot of the esp32. This loop is used to save data in the esp32 EEPROM.
    * The esp32 memory map is in the data. 
    * The App software is need to check the length of the sended data. It must be lower than 50 chars.
+   * In that configuration the info led will blink in green every second. 
    */
-  bool run = true;
+  
   Serial.begin(115200);
   Serial.setTimeout(2000);
+
   int writen = 0;
   char tmp[50];
   char action;
-  Serial.println("Serial");  
+  bool run = true;
+  unsigned long previousMillis = 0;
+  const int delay = 1000;
+
   while(run){
-    // µc will receive data from the app through UART. Those will be stored in the EEPROM folowing the memory table that has been draw.
+    /* µc will receive data from the app through UART. 
+     * Those will be stored in the EEPROM folowing the memory table that has been draw.
+     * The 48 char represent the action to do.
+     */
+
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis >= delay) {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
+      digitalWrite(infoPinG, !digitalRead(infoPinG));
+    }
+
     if(Serial.available()){
       String str = Serial.readStringUntil('%');
       str.toCharArray(tmp, 50);
-      Serial.println(tmp);
       action = tmp[48];
-      Serial.println(action);
       Serial.read();
     }
     switch (action){
@@ -91,22 +132,24 @@ void setupWifiData(){
         break;   
     }
     action = 'n';
-    if(writen == 2){
+    if(writen == 4){
       run = false;
       EEPROM.write(511, 0x55);
     }
   }
+  digitalWrite(infoPinG, HIGH);
   EEPROM.commit();
   ESP.restart();
 }
 
 void resetArray(char* arrayToReset){
-  // Take a pointer array as argument and reset it to ' '.
+  // Take a pointer array as argument and reset it to Null char (0).
   for (int i = 0; i < 50; ++i)
-    arrayToReset[i] = ' ';
+    arrayToReset[i] = 0;
 }
 
 void EEPROM_read(char* arrayToRead, int firstIndex){
+  // Read the EEPROM from firstIndex and write readed data into the arrayToRead
   for(int i = 0; i<50; i++){
     if(EEPROM.read(firstIndex+i) != ' '){
       arrayToRead[i] = EEPROM.read(firstIndex+i);
@@ -117,6 +160,8 @@ void EEPROM_read(char* arrayToRead, int firstIndex){
 }
 
 void EEPROM_write(char arrayToWrite[], int firstIndex){
+  // Write the arrayToWrite array in the EEPROM starting at the firstIndex index.
+
   for(int i= 0; i<50; i++){
     if(arrayToWrite[i] != ' '){
       EEPROM.write(firstIndex+i, arrayToWrite[i]);
